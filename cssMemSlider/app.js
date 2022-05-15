2ab5e0609ebb5db5f42063824e0d728b8456ca6a
Evgeen1981 <66670722+Evgeen1981@users.noreply.github.com>
2022-05-15 22:03:32 +0300

    feat: add change image functional (Sun, May 15, 2022 10:03:32 PM)

--- cssMemSlider/app.js
+++ cssMemSlider/app.js
@@ -1,1 +1,20 @@
 import list from './list.js';
+
+const sliderImg = document.querySelector('.slider__img');
+const sliderText = document.querySelector('.slider__text');
+const sliderControls = document.querySelector('.slider__controls');
+const sliderControl = document.querySelectorAll('.slider__control');
+
+function changeContent() {
+  sliderControls.addEventListener('click', (event) => {
+    sliderControl.forEach(btn => {
+      btn.classList.remove('slider__control--active');
+    })
+    event.target.classList.add('slider__control--active');
+    const num = event.target.dataset.num;
+    sliderImg.src = `${list[num - 1]["img"]}`;
+    sliderText.textContent = `${list[num - 1]["text"]}`;
+  })
+}
+
+changeContent();
